1e21a717219889319f2fc312e785dddc94671413
Your Name <you@example.com>
2026-04-17 22:52:36 +0400

    Fix: disabled timestamps for Hero and Item models

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     protected $fillable = ['name', 'info', 'photo'];
+    // Отключаем обязательные столбцы времени
+    public $timestamps = false;
 }
